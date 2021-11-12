d3309023cf97d3cd4e5d05c722d10f8962e6b2d0
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-12 12:01:29 +0300

    update gendiff.js: add format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,10 @@ const program = new Command();
 
 program
   .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information');
+
+program.option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
