30b9c896a66279c7af3d34a86e34b94523f8a819
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-12 18:46:02 +0300

    update gendiff.js: create gendiff function, export default gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,7 @@ import { readFileSync } from 'fs';
 import _ from 'lodash';
 import * as path from 'path';
 
+// node index.js file1.json file2.json  
 // test: gendiff ./tests/file1.json ./tests/file2.json
 const program = new Command();
 
@@ -42,33 +43,37 @@ const compareFiles = (file1, file2) => {
   console.log(resultString);
 };
 
-program
-  .version('0.0.1')
-  .arguments('<filepath1> <filepath2>')
-  .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information')
-  .action((filepath1, filepath2) => {
-    const absPathOfFile1 = path.resolve(process.cwd(), filepath1);
-    const absPathOfFile2 = path.resolve(process.cwd(), filepath2);
+const gendiff = () => {
+  program
+    .version('0.0.1')
+    .arguments('<filepath1> <filepath2>')
+    .description('Compares two configuration files and shows a difference.')
+    .helpOption('-h, --help', 'output usage information')
+    .action((filepath1, filepath2) => {
+      const absPathOfFile1 = path.resolve(process.cwd(), filepath1);
+      const absPathOfFile2 = path.resolve(process.cwd(), filepath2);
 
-    const fileOneString = readFileSync(absPathOfFile1, 'utf-8', (err, data) => {
-      if (err) throw err;
-      return JSON.parse(data);
+      const fileOneString = readFileSync(absPathOfFile1, 'utf-8', (err, data) => {
+        if (err) throw err;
+        return JSON.parse(data);
+      });
+
+      const fileTwoString = readFileSync(absPathOfFile2, 'utf-8', (err, data) => {
+        if (err) throw err;
+        return JSON.parse(data);
+      });
+
+      const file1 = JSON.parse(fileOneString);
+      const file2 = JSON.parse(fileTwoString);
+
+      compareFiles(file1, file2);
     });
 
-    const fileTwoString = readFileSync(absPathOfFile2, 'utf-8', (err, data) => {
-      if (err) throw err;
-      return JSON.parse(data);
-    });
+  program.option('-f, --format [type]', 'output format');
 
-    const file1 = JSON.parse(fileOneString);
-    const file2 = JSON.parse(fileTwoString);
+  program.parse(process.argv);
+};
 
-    compareFiles(file1, file2);
-  });
+gendiff();
 
-program.option('-f, --format [type]', 'output format');
-
-program.parse(process.argv);
-
-export default compareFiles;
+export default gendiff;
