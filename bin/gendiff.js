8b6b95b23985f011f3a2757bec1010f440e672d3
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-12 17:03:18 +0300

    update: gendiff.js shows files difference in console

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,36 +4,42 @@ import { readFileSync } from 'fs';
 import _ from 'lodash';
 import * as path from 'path';
 
+// test: gendiff ./tests/file1.json ./tests/file2.json
 const program = new Command();
 
 const compareFiles = (file1, file2) => {
-  // console.log(file1);
-  // console.log(file2);
   const result = {};
+
   const keys1 = Object.keys(file1);
   const keys2 = Object.keys(file2);
+  const allKeys = _.uniq(keys1.concat(keys2)).sort();
   const commonKeys = _.intersection(keys1, keys2);
   const uniqueKeys1 = _.difference(keys1, keys2);
   const uniqueKeys2 = _.difference(keys2, keys1);
 
-  // console.log(commonKeys);
-  // console.log(uniqueKeys1);
-  // console.log(uniqueKeys2);
-
-  // console.log(keys1);
-  // console.log(keys2);
-
-  if (keys1.length > keys2.length) {
-    /* eslint-disable-next-line */
-    for (const key of keys1) {
+  allKeys.map((key) => {
+    if (commonKeys.includes(key)) {
       if (file1[key] === file2[key]) {
         result[`  ${key}`] = file1[key];
-      } else if (file1[key] !== file2[key] && _.has(file2, key)) {
-        result[`+ ${key}`] = file1[key];
-        result[`- ${key}`] = file2[key];
+      } else {
+        result[`- ${key}`] = file1[key];
+        result[`+ ${key}`] = file2[key];
       }
     }
-  }
+
+    if (uniqueKeys1.includes(key)) {
+      result[`- ${key}`] = file1[key];
+    }
+
+    if (uniqueKeys2.includes(key)) {
+      result[`+ ${key}`] = file2[key];
+    }
+
+    return undefined;
+  });
+
+  const resultString = JSON.stringify(result, null, 2).replaceAll('"', '');
+  console.log(resultString);
 };
 
 program
@@ -54,8 +60,6 @@ program
       if (err) throw err;
       return JSON.parse(data);
     });
-
-    // console.log(strFile1, strFile2);
 
     const file1 = JSON.parse(fileOneString);
     const file2 = JSON.parse(fileTwoString);
