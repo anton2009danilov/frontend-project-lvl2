f18dd3b4e7b1c789a3057ce63c8f8b8e139e9a46
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-12 10:20:56 +0300

    update: gendiff --help command

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,7 @@ const program = new Command();
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information');
 
 program.parse(process.argv);
