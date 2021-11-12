05ca3ab3885491894f3dbb8d186fc1c67e539147
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-12 14:29:01 +0300

    update: gendiff reads absolute and relative paths

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
 import { readFileSync } from 'fs';
+import * as path from 'path';
 
 const program = new Command();
 
@@ -10,12 +11,15 @@ program
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .action((filepath1, filepath2) => {
-    const file1 = readFileSync(filepath1, 'utf-8', (err, data) => {
+    const absPathOfFile1 = path.resolve(process.cwd(), filepath1);
+    const absPathOfFile2 = path.resolve(process.cwd(), filepath2);
+
+    const file1 = readFileSync(absPathOfFile1, 'utf-8', (err, data) => {
       if (err) throw err;
       return JSON.parse(data);
     });
 
-    const file2 = readFileSync(filepath2, 'utf-8', (err, data) => {
+    const file2 = readFileSync(absPathOfFile2, 'utf-8', (err, data) => {
       if (err) throw err;
       return JSON.parse(data);
     });
