9972a05245468f59b19187e3d9206d74cc003ef1
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-14 12:00:44 +0300

    update gendeff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,28 +1,27 @@
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
-// import action from '../bin/action';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+import action from '../bin/action';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-// const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-// const fixture1 = getFixturePath('file1.json');
-// const fixture2 = getFixturePath('file2.json');
-const path1 = './__fixtures__/file1.json';
-const path2 = './__fixtures__/file2.json';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const fixture1 = getFixturePath('file1.json');
+const fixture2 = getFixturePath('file2.json');
+// const path1 = './__fixtures__/file1.json';
+// const path2 = './__fixtures__/file2.json';
 
-// const diff = `{
-//   - follow: false
-//     host: hexlet.io
-//   - proxy: 123.234.53.22
-//   - timeout: 50
-//   + timeout: 20
-//   + verbose: true
-// }`;
+const diff = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
 
 // eslint-disable-next-line
 test('gendiff', () => {
-  console.log(path1, path2);
+  console.log(fixture1, fixture2);
   // eslint-disable-next-line
-  expect(1).toBe(1);
-  // expect(action(path1, path2)).toBe(diff);
+  expect(action(fixture1, fixture2)).toBe(diff);
 });
