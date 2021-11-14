f1fffddfe939906a9d2be99c2bcef089d337359e
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-14 10:40:38 +0300

    debagging make test-coverage 3

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,6 +1,6 @@
 // import { fileURLToPath } from 'url';
 // import { dirname } from 'path';
-import action from '../bin/action';
+// import action from '../bin/action';
 
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = dirname(__filename);
