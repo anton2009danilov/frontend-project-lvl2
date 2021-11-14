92dbda1d21b6330016fd112a0d0cf6fb5f38daad
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-14 17:48:58 +0300

    add test for yaml fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,8 +6,6 @@ import action from '../bin/action';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const fixture1 = getFixturePath('file1.json');
-const fixture2 = getFixturePath('file2.json');
 const resultPath = getFixturePath('result.txt');
 
 const result = readFileSync(resultPath, 'utf-8', (err, data) => {
@@ -17,6 +15,16 @@ const result = readFileSync(resultPath, 'utf-8', (err, data) => {
 
 // eslint-disable-next-line
 test('gendiff', () => {
+  const fixture1 = getFixturePath('file1.json');
+  const fixture2 = getFixturePath('file2.json');
+  // eslint-disable-next-line
+  expect(action(fixture1, fixture2)).toBe(result);
+});
+
+// eslint-disable-next-line
+test('gendiff yaml test', () => {
+  const fixture1 = getFixturePath('file1.yaml');
+  const fixture2 = getFixturePath('file2.yaml');
   // eslint-disable-next-line
   expect(action(fixture1, fixture2)).toBe(result);
 });
