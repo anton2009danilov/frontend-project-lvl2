3c7f772837325b5dad71047442c8259b45b0755b
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-14 15:27:15 +0300

    create and plug result fixture

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,3 +1,4 @@
+import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import * as path from 'path';
 import action from '../bin/action';
@@ -7,21 +8,15 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const fixture1 = getFixturePath('file1.json');
 const fixture2 = getFixturePath('file2.json');
-// const path1 = './__fixtures__/file1.json';
-// const path2 = './__fixtures__/file2.json';
+const resultPath = getFixturePath('result.txt');
 
-const diff = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+const result = readFileSync(resultPath, 'utf-8', (err, data) => {
+  if (err) throw err;
+  return JSON.parse(data);
+});
 
 // eslint-disable-next-line
 test('gendiff', () => {
-  console.log(fixture1, fixture2);
   // eslint-disable-next-line
-  expect(action(fixture1, fixture2)).toBe(diff);
+  expect(action(fixture1, fixture2)).toBe(result);
 });
