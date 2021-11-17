b22fa58832b0be68f313ea768f416c193d8ff97c
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-17 16:03:08 +0300

    finish compareAll function

--- bin/action.js
+++ bin/action.js
@@ -5,9 +5,13 @@ import parse from './parsers.js';
 import stringify from './stringify.js';
 
 const buildTree = (data) => {
-  // console.log(Object.entries(data));
   const tree = {};
+  console.log(data);
+  if (data === null) {
+    return data;
+  }
 
+  console.log(Object.entries(data));
   Object.entries(data).map(([itemName, itemValue]) => {
     const type = typeof itemValue === 'object' ? 'tree' : 'item';
     const value = typeof itemValue === 'object' ? buildTree(itemValue) : itemValue;
@@ -20,8 +24,6 @@ const buildTree = (data) => {
     return undefined;
   });
 
-  // console.log(data);
-  // console.log(JSON.stringify(tree, null, 2));
   return tree;
 };
 
@@ -40,22 +42,28 @@ const compareAll = (data1, data2) => {
     }
 
     if (node1.type === 'tree' && node2.type === 'tree') {
-      // console.log(node1);
-      // console.log(node2);
       return compareAll(node1.value, node2.value);
     }
 
     result.type = 'list';
-    result[`file1__${key}`] = typeof node1 === 'object' ? _.cloneDeep(node1) : node1;
-    result[`file2__${key}`] = typeof node2 === 'object' ? _.cloneDeep(node2) : node2;
-    result[`file1__${key}`].sign = '-';
-    result[`file2__${key}`].sign = '+';
+    result.before = typeof node1 === 'object' ? _.cloneDeep(node1) : node1;
+    result.after = typeof node2 === 'object' ? _.cloneDeep(node2) : node2;
+    result.before.sign = '-';
+    result.after.sign = '+';
     return result;
   };
 
-const compareUnique = (key, node, sign) => {
+  const compareUnique = (key, node, sign) => {
+    let result;
+    if (node.type === 'tree') {
+      result = _.cloneDeep(node);
+    } else {
+      result = node;
+    }
 
-};
+    result.sign = sign;
+    return result;
+  };
 
   const result = {};
   const keys1 = Object.keys(data1);
@@ -112,7 +120,7 @@ const action = (filepath1, filepath2) => {
   const tree1 = buildTree(file1);
   const tree2 = buildTree(file2);
   compareAll(tree1, tree2);
-  // return stringify(compareFiles(file1, file2), ' ', 2).replace(/"/g, '').replace(/,/g, '');
+  // return stringify(compareAll(tree1, tree2), ' ', 2).replace(/"/g, '').replace(/,/g, '');
 };
 
 export default action;
