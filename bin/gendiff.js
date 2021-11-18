49a379450c5d9d5dcf3004c53720970d51034579
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-18 15:38:18 +0300

    update gendiff.js: set stylish as default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
-import { Command } from 'commander/esm.mjs';
+import { Command, Option } from 'commander/esm.mjs';
 import action from './action.js';
 
 const gendiff = () => {
   const program = new Command();
+  const options = program.opts();
+  const formatOption = new Option('-f, --format [type]', 'output format')
+    .default('stylish')
+    .choices(['stylish']);
+
   program
     .version('0.0.1')
     .arguments('<filepath1> <filepath2>')
     .description('Compares two configuration files and shows a difference.')
     .helpOption('-h, --help', 'output usage information')
-    .action((path1, path2) => console.log(action(path1, path2)));
-
-  program.option('-f, --format [type]', 'output format');
+    .addOption(formatOption)
+    .action((path1, path2) => console.log(action(path1, path2, options.format)));
 
   program.parse(process.argv);
 };
