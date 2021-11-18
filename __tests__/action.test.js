dd1559036d9c95818599b781f9d7d51a475e5d88
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-18 19:19:30 +0300

    create test for gendiff with plain formatter

--- __tests__/action.test.js
+++ __tests__/action.test.js
@@ -6,9 +6,15 @@ import action from '../bin/action';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const resultPath = getFixturePath('result.txt');
+const stylishResultPath = getFixturePath('stylish_result.txt');
+const plainResultPath = getFixturePath('plain_result.txt');
 
-const result = readFileSync(resultPath, 'utf-8', (err, data) => {
+const stylishResult = readFileSync(stylishResultPath, 'utf-8', (err, data) => {
+  if (err) throw err;
+  return JSON.parse(data);
+});
+
+const plainResult = readFileSync(stylishResultPath, 'utf-8', (err, data) => {
   if (err) throw err;
   return JSON.parse(data);
 });
@@ -18,7 +24,7 @@ test('gendiff', () => {
   const fixture1 = getFixturePath('file1.json');
   const fixture2 = getFixturePath('file2.json');
   // eslint-disable-next-line
-  expect(action(fixture1, fixture2)).toBe(result);
+  expect(action(fixture1, fixture2)).toBe(stylishResult);
 });
 
 // eslint-disable-next-line
@@ -26,7 +32,7 @@ test('gendiff yaml test', () => {
   const fixture1 = getFixturePath('file1.yaml');
   const fixture2 = getFixturePath('file2.yaml');
   // eslint-disable-next-line
-  expect(action(fixture1, fixture2)).toBe(result);
+  expect(action(fixture1, fixture2)).toBe(stylishResult);
 });
 
 // eslint-disable-next-line
@@ -34,5 +40,13 @@ test('gendiff yaml and json test', () => {
   const fixture1 = getFixturePath('file1.json');
   const fixture2 = getFixturePath('file2.yaml');
   // eslint-disable-next-line
-  expect(action(fixture1, fixture2)).toBe(result);
+  expect(action(fixture1, fixture2)).toBe(stylishResult);
+});
+
+// eslint-disable-next-line
+test('gendiff stylish json test', () => {
+  const fixture1 = getFixturePath('file1.json');
+  const fixture2 = getFixturePath('file2.json');
+  // eslint-disable-next-line
+  expect(action(fixture1, fixture2, 'plain')).toBe(plainResult);
 });
