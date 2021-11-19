370dc30c1a0ee9d27a5750dc95887e2cfec90b7d
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-19 14:24:49 +0300

    update plain.js: create stringify func, now plain formatter is finished

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,6 +1,36 @@
 const isObject = (data) => Object.prototype.toString.call(data) === '[object Object]';
 const getSimpleValue = (item) => (isObject(item.value) ? '[complex value]' : item.value);
 
+const prepareForDisplay = (value) => (typeof value === 'string' && value !== '[complex value]'
+  ? `'${value}'`
+  : value);
+
+const stringify = (data, str = '', parentKey = '') => {
+  let result = str;
+
+  Object.entries(data).forEach(([key, item]) => {
+    const currentKey = parentKey ? `${parentKey}.${key}` : key;
+
+    if (!item.status) {
+      result = stringify(item, result, currentKey);
+    }
+
+    if (item.status === 'added') {
+      result += `Property '${currentKey}' was ${item.status} with value: ${prepareForDisplay(item.value)}\n`;
+    }
+
+    if (item.status === 'removed') {
+      result += `Property '${currentKey}' was ${item.status}\n`;
+    }
+
+    if (item.status === 'updated') {
+      result += `Property '${currentKey}' was ${item.status}. From ${prepareForDisplay(item.before)} to ${prepareForDisplay(item.after)}\n`;
+    }
+  });
+
+  return result;
+};
+
 const format = (data) => {
   const formattedResult = {};
   Object.entries(data).forEach(([key, item]) => {
@@ -45,4 +75,6 @@ const format = (data) => {
   return formattedResult;
 };
 
-export default format;
+const plain = (data) => stringify(format(data)).replace(/\n$/, '');
+
+export default plain;
