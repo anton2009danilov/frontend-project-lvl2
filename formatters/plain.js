7ef99fdf11643534328cde47708e5a7e17092b32
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-19 11:39:50 +0300

    update formatters/plain.js: format function return reformatted diffs tree

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,5 +1,48 @@
-const format = (data) => {
+const isObject = (data) => Object.prototype.toString.call(data) === '[object Object]';
+const getSimpleValue = (item) => (isObject(item.value) ? '[complex value]' : item.value);
 
+const format = (data) => {
+  const formattedResult = {};
+  Object.entries(data).forEach(([key, item]) => {
+    if (key === 'type') return;
+
+    let result = {};
+
+    if (item.type === 'tree' && !item.sign) {
+      if (item.value) {
+        if (item.value.type === 'tree') {
+          result.child = format(item);
+        }
+      }
+      result = format(item);
+    }
+
+    if (item.type === 'tree' && item.sign) {
+      result.status = item.sign === '+' ? 'added' : 'removed';
+
+      if (item.sign === '+') {
+        result.value = getSimpleValue(item);
+      }
+    }
+
+    if (item.type === 'item' && item.sign) {
+      result.status = item.sign === '+' ? 'added' : 'removed';
+
+      if (item.sign === '+') {
+        result.value = item.value;
+      }
+    }
+
+    if (item.type === 'list') {
+      result.status = 'updated';
+      result.before = getSimpleValue(item.before);
+      result.after = getSimpleValue(item.after);
+    }
+
+    formattedResult[key] = result;
+  });
+
+  return formattedResult;
 };
 
 export default format;
