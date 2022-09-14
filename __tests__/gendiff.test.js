d36fe53d14e95e650b95212a78567c0055b57b42
Anton Danilov <anton2009danilov@yandex.ru>
2022-09-14 12:33:18 +0300

    fixed: tests using eslint-disable-next-line command

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import * as path from 'path';
-import genDiff from '../lib/gendiff.js';
+import genDiff from '../src/gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,42 +25,32 @@ const jsonResult = readFileSync(jsonResultPath, 'utf-8', (err, data) => {
   return JSON.parse(data);
 });
 
-// eslint-disable-next-line
 test('gendiff file1.json file2.json', () => {
   const fixture1 = getFixturePath('file1.json');
   const fixture2 = getFixturePath('file2.json');
-  // eslint-disable-next-line
   expect(genDiff(fixture1, fixture2)).toBe(stylishResult);
 });
 
-// eslint-disable-next-line
 test('gendiff file1.yaml file2.yaml', () => {
   const fixture1 = getFixturePath('file1.yaml');
   const fixture2 = getFixturePath('file2.yaml');
-  // eslint-disable-next-line
   expect(genDiff(fixture1, fixture2)).toBe(stylishResult);
 });
 
-// eslint-disable-next-line
 test('gendiff file1.json file2.yaml', () => {
   const fixture1 = getFixturePath('file1.json');
   const fixture2 = getFixturePath('file2.yaml');
-  // eslint-disable-next-line
   expect(genDiff(fixture1, fixture2)).toBe(stylishResult);
 });
 
-// eslint-disable-next-line
 test('gendiff -f plain file1.json file2.json', () => {
   const fixture1 = getFixturePath('file1.json');
   const fixture2 = getFixturePath('file2.json');
-  // eslint-disable-next-line
   expect(genDiff(fixture1, fixture2, 'plain')).toBe(plainResult);
 });
 
-// eslint-disable-next-line
 test('gendiff -f json file1.json file2.json', () => {
   const fixture1 = getFixturePath('file1.json');
   const fixture2 = getFixturePath('file2.json');
-  // eslint-disable-next-line
   expect(genDiff(fixture1, fixture2, 'json')).toBe(jsonResult);
 });
