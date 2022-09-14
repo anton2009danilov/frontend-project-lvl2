3a334918868bf8f4bbe02c78316874e4749d2c05
Anton Danilov <anton2009danilov@yandex.ru>
2022-09-14 21:23:59 +0300

    refactor: split plain.js stringify function into few smaller functions

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -13,22 +13,32 @@ const prepareForDisplay = (value) => {
   return value;
 };
 
+const formatAdded = (resultStr, currentKey, type, currentValue) => `${resultStr}Property '${currentKey}' was ${type} with value: ${prepareForDisplay(currentValue)}\n`;
+
+const formatRemoved = (resultStr, currentKey, type) => `${resultStr}Property '${currentKey}' was ${type}\n`;
+
+const formatUpdated = (resultStr, currentKey, item) => {
+  const { type, before, after } = item;
+
+  return `${resultStr}Property '${currentKey}' was ${type}. From ${prepareForDisplay(before)} to ${prepareForDisplay(after)}\n`;
+};
+
 const stringify = (data, str = '', parentKey = '') => data.reduce((resultStr, item) => {
   const {
-    name, type, value, children, before, after,
+    name, type, value, children,
   } = item;
-  const currentValue = value === undefined ? getSimpleValue(children) : value;
+  const currentValue = (value === undefined) ? getSimpleValue(children) : value;
   const currentKey = parentKey ? `${parentKey}.${name}` : name;
 
   switch (type) {
     case 'added':
-      return `${resultStr}Property '${currentKey}' was ${type} with value: ${prepareForDisplay(currentValue)}\n`;
+      return formatAdded(resultStr, currentKey, type, currentValue);
     case 'removed':
-      return `${resultStr}Property '${currentKey}' was ${type}\n`;
+      return formatRemoved(resultStr, currentKey, type);
     case 'updated':
-      return `${resultStr}Property '${currentKey}' was ${type}. From ${prepareForDisplay(before)} to ${prepareForDisplay(after)}\n`;
+      return formatUpdated(resultStr, currentKey, item);
     default:
-      return children !== undefined ? stringify(children, resultStr, currentKey) : resultStr;
+      return (children !== undefined) ? stringify(children, resultStr, currentKey) : resultStr;
   }
 }, str);
 
