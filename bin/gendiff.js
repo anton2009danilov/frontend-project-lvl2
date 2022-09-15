a4c9eb7f9beec0abeda77564f46e0e7d6633f85f
Anton Danilov <anton2009danilov@yandex.ru>
2022-09-15 09:27:19 +0300

    refactor: get rid of unnecessary function wrapping

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,22 +2,18 @@
 import { Command, Option } from 'commander/esm.mjs';
 import action from '../src/gendiff.js';
 
-const gendiff = () => {
-  const program = new Command();
-  const options = program.opts();
-  const formatOption = new Option('-f, --format [type]', 'output format')
-    .default('stylish')
-    .choices(['stylish', 'plain', 'json']);
+const program = new Command();
+const options = program.opts();
+const formatOption = new Option('-f, --format [type]', 'output format')
+  .default('stylish')
+  .choices(['stylish', 'plain', 'json']);
 
-  program
-    .version('0.0.1')
-    .arguments('<filepath1> <filepath2>')
-    .description('Compares two configuration files and shows a difference.')
-    .helpOption('-h, --help', 'output usage information')
-    .addOption(formatOption)
-    .action((path1, path2) => console.log(action(path1, path2, options.format)));
+program
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .addOption(formatOption)
+  .action((path1, path2) => console.log(action(path1, path2, options.format)));
 
-  program.parse(process.argv);
-};
-
-gendiff();
+program.parse(process.argv);
