37de5e29998c33261f6a847429447438b5745693
Anton Danilov <anton2009danilov@yandex.ru>
2022-09-21 13:33:29 +0300

    refactor: buildTree function optimization

--- src/buildTree.js
+++ src/buildTree.js
@@ -8,83 +8,12 @@ const formatItemChildren = (item, type = 'unchanged') => Object.entries(item).ma
   return ({ name, value, type });
 });
 
-const updateItem = (item1, item2, buildType, currentName = undefined) => {
-  switch (buildType) {
-    case 'tree vs value':
-      return {
-        name: currentName,
-        before: formatItemChildren(item1),
-        after: item2,
-        type: 'updated',
-      };
-    case 'value vs tree':
-      return {
-        name: currentName,
-        before: item1,
-        after: formatItemChildren(item2),
-        type: 'updated',
-      };
-    case 'value vs value':
-      return {
-        name: currentName,
-        before: item1,
-        after: item2,
-        type: 'updated',
-      };
-    default:
-      throw new Error(`Unexpected value of buildType variable: ${buildType}`);
-  }
-};
-
 const defineTreeChangeType = (item1, item2) => {
   if (_.isEqual(item1, item2)) {
     return 'unchanged';
   }
 
   return 'children updated';
-};
-
-const compareWithEmptiness = (item, buildType, name) => {
-  switch (buildType) {
-    case 'emptiness vs tree':
-      return { name, children: formatItemChildren(item), type: 'added' };
-    case 'emptiness vs value':
-      return { name, value: item, type: 'added' };
-    case 'tree vs emptiness':
-      return { name, children: formatItemChildren(item), type: 'removed' };
-    case 'value vs emptiness':
-      return { name, value: item, type: 'removed' };
-    default:
-      throw new Error(`Unexpected value of buildType variable: ${buildType}`);
-  }
-};
-
-const defineBuildType = (item1, item2) => {
-  if (_.isObject(item1) && _.isObject(item2)) {
-    return 'tree vs tree';
-  }
-
-  if (item1 === undefined) {
-    return _.isObject(item2) ? 'emptiness vs tree' : 'emptiness vs value';
-  }
-
-  if (item2 === undefined) {
-    return _.isObject(item1) ? 'tree vs emptiness' : 'value vs emptiness';
-  }
-
-  if (_.isEqual(item1, item2)) {
-    return 'value equal value';
-  }
-
-  if (_.isObject(item1)) {
-    return 'tree vs value';
-  }
-
-  if (_.isObject(item2)) {
-    return 'value vs tree';
-  }
-
-  return 'value vs value';
 };
 
 const buildDifferencesTree = (file1, file2) => {
@@ -94,30 +23,54 @@ const buildDifferencesTree = (file1, file2) => {
     const item1 = file1[currentName];
     const item2 = file2[currentName];
 
-    const buildType = defineBuildType(item1, item2);
-
-    switch (buildType) {
-      case 'value equal value':
-        return [...root, { name: currentName, value: item1, type: 'unchanged' }];
-      case 'tree vs tree':
-        return [...root, {
-          name: currentName,
-          children: buildDifferencesTree(item1, item2),
-          type: defineTreeChangeType(item1, item2),
-        }];
-      case 'emptiness vs tree':
-      case 'emptiness vs value':
-        return [...root, compareWithEmptiness(item2, buildType, currentName)];
-      case 'tree vs emptiness':
-      case 'value vs emptiness':
-        return [...root, compareWithEmptiness(item1, buildType, currentName)];
-      case 'tree vs value':
-      case 'value vs tree':
-      case 'value vs value':
-        return [...root, updateItem(item1, item2, buildType, currentName)];
-      default:
-        throw new Error(`Unexpected value of buildType variable: ${buildType}`);
+    if (_.isEqual(item1, item2)) {
+      return [...root, { name: currentName, value: item1, type: 'unchanged' }];
     }
+
+    if (_.isObject(item1) && _.isObject(item2)) {
+      return [...root, {
+        name: currentName,
+        children: buildDifferencesTree(item1, item2),
+        type: defineTreeChangeType(item1, item2),
+      }];
+    }
+
+    if (item1 === undefined) {
+      return _.isObject(item2)
+        ? [...root, { name: currentName, children: formatItemChildren(item2), type: 'added' }]
+        : [...root, { name: currentName, value: item2, type: 'added' }];
+    }
+
+    if (item2 === undefined) {
+      return _.isObject(item1)
+        ? [...root, { name: currentName, children: formatItemChildren(item1), type: 'removed' }]
+        : [...root, { name: currentName, value: item1, type: 'removed' }];
+    }
+
+    if (_.isObject(item1)) {
+      return [...root, {
+        name: currentName,
+        before: formatItemChildren(item1),
+        after: item2,
+        type: 'updated',
+      }];
+    }
+
+    if (_.isObject(item2)) {
+      return [...root, {
+        name: currentName,
+        before: item1,
+        after: formatItemChildren(item2),
+        type: 'updated',
+      }];
+    }
+
+    return [...root, {
+      name: currentName,
+      before: item1,
+      after: item2,
+      type: 'updated',
+    }];
   }, []);
 
   return resultTree;
